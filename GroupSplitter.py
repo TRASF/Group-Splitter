ba874153b9a4ec8c3aef1b4fd458c3cc68967262
TRASF <thantamrong28@gmail.com>
2023-07-18 11:14:17 +0200

    round_pushpin hot-fix GroupSplitter

--- GroupSplitter.py
+++ GroupSplitter.py
@@ -2,50 +2,77 @@ import pandas as pd
 import random, os
 
 OUTPUT_DIR = 'output/'
-FILE_NAME = 'MOCK_DATA.xlsx'
-FAC_COL_NAME = 'Faculty'
-NUM_GROUPS = 2
-
+FILE_NAME = 'FILE.xlsx'
+FAC_COL_NAME = 'faculty'
+NUM_GROUPS = 28
+FACTOR = 5
 random.seed(42)
 
 # Read the Excel file into a DataFrame
 data = pd.read_excel(FILE_NAME)
 
-# Group the employees by department and count the number of employees in each department
-nongNong_fac_count = data[FAC_COL_NAME].value_counts()
-nongNong_total_count = data.shape[0]
+# Group the students by department and count the number of students in each department
+nongNong_counts = data[FAC_COL_NAME].value_counts()
 
 # Display the statistics
-# print("Faculty\tNongNong count")
-
-group_size = len(data) // NUM_GROUPS
-print(f"Count nongNong = {nongNong_total_count}")
-print(f"Group needed = {NUM_GROUPS} | Group size = {group_size}" + "\n---" )
+print("Department\tNongNong count")
+print(nongNong_counts)
 
 # Initialize empty groups
 groups = [[] for _ in range(NUM_GROUPS)]
 
-# Distribute employees equally across the groups
-for department, count in nongNong_fac_count.items():
-    nongNongs = data[data[FAC_COL_NAME] == department].index.tolist()
-    random.shuffle(nongNongs)
+# Separate departments with fewer students than the desired minimum group size
+min_group_size = NUM_GROUPS + FACTOR  # Set the desired minimum group size
+small_departments = nongNong_counts[
+    (nongNong_counts <= min_group_size) & (nongNong_counts > 0)
+].index.tolist()
 
-    for i, nongNong in enumerate(nongNongs):
+random.shuffle(small_departments)
+
+# Distribute students from small departments to individual groups
+for department in small_departments:
+    department_students = data[data[FAC_COL_NAME] == department].index.tolist()
+
+    # Randomly choose a group until we find one that is not full
+    while True:
+        group_index = random.randint(0, NUM_GROUPS-1)
+        if len(groups[group_index]) < min_group_size:
+            break
+
+    groups[group_index].extend(department_students)
+
+    # Remove the small department from the nongNong_counts
+    nongNong_counts.drop(department, inplace=True)
+
+
+# Calculate the desired group size after accounting for small departments
+group_size = (len(data) - len(small_departments)) // NUM_GROUPS
+
+# Distribute the remaining students across the groups
+for department, count in nongNong_counts.items():
+    students = data[data[FAC_COL_NAME] == department].index.tolist()
+    random.shuffle(students)
+
+    # Distribute students to each group
+    for i, student in enumerate(students):
         group_index = i % NUM_GROUPS
-        groups[group_index].append(nongNong)
+        groups[group_index].append(student)
 
-# Allocate any remaining employees randomly to groups
-remaining_nongNongs = data[
-    ~data.index.isin(
-        [assigned_nongNong for group in groups for assigned_nongNong in group]
-    )
-]
+# Calculate the target size for each group
+target_size = (len(data) - len(small_departments)) // NUM_GROUPS
 
-random.shuffle(remaining_nongNongs.index)
+# Balance the group sizes
+for group_num, group in enumerate(groups):
+    while len(group) > target_size:
+        # Find a random student in the group
+        student_to_remove = random.choice(group)
 
-for i, nongNong in enumerate(remaining_nongNongs.index, start=1):
-    group_index = i % NUM_GROUPS
-    groups[group_index].append(nongNong)
+        # Remove the student from the group
+        group.remove(student_to_remove)
+
+        # Find another group to transfer the student
+        transfer_group_num = (group_num + 1) % NUM_GROUPS
+        groups[transfer_group_num].append(student_to_remove)
 
 # create directory for output
 os.makedirs('output', exist_ok=True)
